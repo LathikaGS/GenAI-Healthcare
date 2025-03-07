c180813ea3ac3b49d08ee36fbae0fa83d3d5cd04
LathikaGS <135726112+LathikaGS@users.noreply.github.com>
2025-03-07 22:44:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-north-1_lerq3sfYS', 
+        userPoolClientId: '4qu5fhqmq8m5j38o2kb2spfv7d', 
+        region: 'eu-north-1' 
+    },
+    api: {
+        invokeUrl: '' 
+    }
+};
